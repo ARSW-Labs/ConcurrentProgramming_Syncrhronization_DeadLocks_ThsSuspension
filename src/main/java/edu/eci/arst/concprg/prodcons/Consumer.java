7bfee4b89453e26b0e503b756d0631b39b8c742f
Fabián Ardila <fabianra97@gmail.com>
2018-01-25 18:31:12 -0500

    Se corrigio la clase Consumer poniendo el take

--- src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -6,6 +6,9 @@
 package edu.eci.arst.concprg.prodcons;
 
 import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -23,12 +26,12 @@ public class Consumer extends Thread{
     @Override
     public void run() {
         while (true) {
-
-            if (queue.size() > 0) {
-                int elem=queue.poll();
-                System.out.println("Consumer consumes "+elem);                                
+            try {
+                int elem = (Integer)((LinkedBlockingQueue)queue).take();
+                System.out.println("Consumer consumes "+elem);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Consumer.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
         }
     }
 }
